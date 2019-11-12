aa215e6a68e78c9bb5cc8d7257ddc8ba041ac1b2
ryuichiikegami <43158827+ryuichiikegami@users.noreply.github.com>
2019-11-12 14:20:59 -0500

    Create Banker.cpp
    cpp for banker class

---
+++ Banker.cpp
@@ -0,0 +1,23 @@
+
+//Banker.cpp
+
+#include"Banker.h"
+
+Banker::Banker(){
+	setDeposit(0);
+}
+	setWithdraw(0);
+}
+void Banker::setDeposit(double deposit)
+{
+	this ->deposit = deposit;
+}
+void Banker::setWithdraw(double name)
+{
+	this -> withdraw = withdraw;
+void Banker::print(){
+	cout <<"This Customer has this information:\n"
+		<<"Deposit amount: "<<deposit<<"\n"
+		<<"Withdraw amount: "<<withdraw<<"\n"
+		<<"ID = "<<ID<<endl;
+}
