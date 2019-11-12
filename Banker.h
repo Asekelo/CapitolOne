2da28d76601b27aaf7b2da3cb7d767d61883eccd
ryuichiikegami <43158827+ryuichiikegami@users.noreply.github.com>
2019-11-12 14:16:27 -0500

    Create Banker.h
    header file for banker class

---
+++ Banker.h
@@ -0,0 +1,20 @@
+
+//Banker.h
+#include<iostream>
+#include"User.h"
+using namespace std;
+#ifndef BANKER_H
+#define BANKER_H
+
+class Banker: public User{
+	private:
+		double deposit;
+    double withdraw;
+	public:
+		Student();
+		void setWithdraw(double);
+    void setDeposit(double;
+		void print();	
+	
+};
+#endif
