59999e87c03432305764cdf7c911f4beb7d17942
Barron Botts <barronbotts@gmail.com>
2019-11-12 16:18:55 -0500

    Update Banker.h
    Reformatted to match the other header files and to future-proof for other files we will be creating.

--- Banker.h
+++ Banker.h
@@ -1,20 +1,27 @@
-
 //Banker.h
+
 #include<iostream>
 #include"User.h"
 using namespace std;
+
 #ifndef BANKER_H
 #define BANKER_H
 
 class Banker: public User{
 	private:
-		double deposit;
-    double withdraw;
+		//encapsulated data members
+
 	public:
+		//constructors
 		Banker();
-		void setWithdraw(double);
-    void setDeposit(double;
-		void print();	
-	
+		Banker(string, string, int);
+		
+		//mutators
+		
+		//accessors
+		
+		//polymorphic
+		string print() const;
 };
+
 #endif
