8301e7ed3e92ed604578e8234732582aa8442c8a
Barron Botts <barronbotts@gmail.com>
2019-11-11 22:54:52 -0500

    Update User.cpp
    Added, changed and formatted all functions for consistency.

--- User.cpp
+++ User.cpp
@@ -1,20 +1,73 @@
 //User.cpp
+
 #include"User.h"
+#include<string>
+#include<sstream>
+using namespace std;
+
+//constructors
 User::User(){
-	setName("");
-	setAge(0);
+	setSecurity(0);
+	setAccountName("");
+	setAccountPassword("");
+	setID(0);
 }
 
-void User::setaccountName(string n){
-	accountName = n;
-}
-void User::setaccountPassword(string p){
-	accountPassword = p;
-}
-void Person::setAge(int i){
-ID = i;
+User::User(string accountName, string accountPassword, int id) {
+	setSecurity(0);
+	setAccountName(accountName);
+	setAccountPassword(accountPassword);
+	setID(id);
 }
 
-void Person::print(){
-	cout <<"This is a User!\n"<<endl;
+//mutators
+void User::setAccountName(string accountName){
+	this -> accountName = accountName;
+}
+
+void User::setAccountPassword(string accountPassword){
+	this -> accountPassword = accountPassword;
+}
+
+void User::setID(int id) {
+	this -> id = id;
+}
+
+void User::setSecurity(int security) {
+	this -> security = security;
+}
+
+//accessors
+string User::getAccountName() const {
+	return accountName;
+}
+
+string User::getAccountPassword() const {
+	return accountPassword;
+}
+
+int User::getID() const {
+	return id;
+}
+
+int User::getSecurity() const {
+	return security;
+}
+
+string User::getSecurityString() const {
+	stringstream secure;
+	secure << security;
+	return secure.str();
+}
+
+string User::getIDString() const {
+	stringstream iD;
+	iD << id;
+	return iD.str();
+}
+
+//polymorphic
+string User::print() const{
+	string message = "ID: " + getIDString() + "\nUsername: " + getAccountName() + "\nPassword: " + getAccountPassword() + "\n";
+	return message;
 }
