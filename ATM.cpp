606837f9c90ad69dac6ddaa27516ea1c1d5b8131
ryuichiikegami <43158827+ryuichiikegami@users.noreply.github.com>
2019-11-14 15:35:54 -0500

    Create ATM.cpp
    this is the atm cpp needs atm header class included

---
+++ ATM.cpp
@@ -0,0 +1,26 @@
+ATM.cpp
+#include "ATM.cpp"
+
+ATM::ATM(){
+	setDeposit(0);
+  setWithdraw(0);
+  setBalance(0);
+}
+void Faculty::setDeposit(double deposit)
+{
+	this ->deposit = deposit;
+}
+void Faculty::setWithdraw(double withdraw)
+{
+	this ->withdraw = withdraw;
+}
+void Faculty::setBalance(double balance)
+{
+	this ->balance = balance;
+}
+void ATM::print(){
+	cout <<"This transaction has this information:\n"
+		<<"Withdraw amount: "<<withdraw<<"\n"
+		<<"Deposit amount: "<<deposit<<"\n"
+		<<"Balance amount = "<<balance<<endl;
+}
