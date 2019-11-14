7b519104782ac83c06c8c0022fc304d962e8d9d2
ryuichiikegami <43158827+ryuichiikegami@users.noreply.github.com>
2019-11-14 16:27:17 -0500

    Create Audit.h
    audit header file for audit class

---
+++ Audit.h
@@ -0,0 +1,17 @@
+//Audit.h
+#include<iostream>
+using namespace std;
+#ifndef AUDIT_H
+#define AUDIT_H
+
+class Audit: public Audit{
+	private:
+		
+	public:
+		Audit();
+		void viewTransactions();
+    void viewAccounts();
+		void print();	
+	
+};
+#endif
