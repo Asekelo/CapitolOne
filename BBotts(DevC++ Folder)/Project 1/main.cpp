56d167af8a908c57a9297fe7a9f393e781279b88
Barron Botts <barronbotts@gmail.com>
2019-11-16 12:46:43 -0500

    Stopping Point 1.
    It's gonna be a loing day. Right now, the atm class is working very well. we use recursion in there.

--- BBotts(DevC++ Folder)/Project 1/main.cpp
+++ BBotts(DevC++ Folder)/Project 1/main.cpp
@@ -2,10 +2,12 @@
 #include "Admin.h"
 #include "Banker.h"
 #include "Customer.h"
+#include "ATM.h"
 using namespace std;
 
 int main(int argc, char** argv) {
 	Customer me("bbotts", "bbotts", 9, "Barron", "Botts", "2425 14th Street NW", 400.39, 14295.73, 06, 11, 1996);
+	atmMenu(me.getSecurity(), &me);
 	cout << me.print();
 	return 0;
 }
